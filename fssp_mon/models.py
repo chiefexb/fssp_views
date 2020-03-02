7f1a4ed326266e35cd8c47ec5dc44b14439da13e
Aleksey Shilo <chief_exb@mail.ru>
2020-03-02 16:14:40 +0300

    1

--- fssp_mon/models.py
+++ fssp_mon/models.py
@@ -53,9 +53,9 @@ class VitrinaValue (models.Model):
     col19 = models.CharField(max_length=1000, null='True', blank='True', verbose_name='Col19')
     col20 = models.CharField(max_length=1000, null='True', blank='True', verbose_name='Col20')
     
-    @property
-    def vitrina_id (self):
-        return str(self.vitrina.id)
+    #property
+    #def vitrina_id (self):
+    #   return str(self.vitrina.id)
 
     def __str__(self):
         return self.osp.full_name+':'+ self.vitrina.__str__()
