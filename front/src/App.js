9def30e86a5b84f11512a91f9f012cdcd5876c94
Aleksey Shilo <chief_exb@mail.ru>
2020-03-02 18:40:52 +0300

    1

--- front/src/App.js
+++ front/src/App.js
@@ -147,7 +147,7 @@ class App extends React.Component {
            
           </TableHead>
          
-           {this.state.data.map(item => (
+           {this.state.data['data'].map(item => (
              <TableBody  key={item.id} title={item.name}>
             <TableRow>
            <TableCell>
