596cae6475beaf299bbccdfb4d11e0b7c3f09541
Aleksey Shilo <chief_exb@mail.ru>
2020-03-02 16:44:54 +0300

    1

--- fssp_mon/urls.py
+++ fssp_mon/urls.py
@@ -37,7 +37,7 @@ class VitrinaFieldViewSet(viewsets.ModelViewSet):
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
-router.register(r'vitrinavalue', VitrinaValueViewSet)
+router.register(r'vitrinavalue', VitrinaValueViewSet,basename='VitrinaValue')
 router.register(r'vitrinafield', VitrinaFieldViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
