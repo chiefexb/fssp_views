00867b7e3d93738844abcdf88a85b2075a12d887
Aleksey Shilo <chief_exb@mail.ru>
2020-03-06 15:06:47 +0300

    Fix

--- fssp_mon/update.py
+++ fssp_mon/update.py
@@ -3,13 +3,22 @@ import json
 import yaml
 import os
 import time
+import sys
 
 
 bd=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 f=open(os.path.join( bd,'../fssp_views_settings/inventory.yml'))
 db_config=yaml.load(f)
 f.close()
+pid = os.getpid() 
+print (pid)
+pid_path=sys.argv[1]
+f=open(pid_path)
+f.write(pid)
+f.close()
+
 while True:
+    
     r=requests.post('http://localhost/worker', data={'secret': db_config['secret'],'method':'update' }  )
     #r=requests.post('http://localhost/webhook', data={'secret': db_config['secret'],'method':'update' }  )
     print (str(r.text) )
