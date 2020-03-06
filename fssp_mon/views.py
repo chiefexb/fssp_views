ae4c81b612764e7666b547839d8aba38b92d6cee
Aleksey Shilo <chief_exb@mail.ru>
2020-03-06 10:22:31 +0300

    Fix

--- fssp_mon/views.py
+++ fssp_mon/views.py
@@ -58,7 +58,7 @@ def swagger (request):
     
 def api2(request,method=None,method2=None):
     if request.method=='POST' and method=='filter' and method2=='category_add':
-        j=request.POST['data']
+        j=dict(request.POST)
         #csrf_token = django.middleware.csrf.get_token()
         return JsonResponse({'rez':  j})
     if method=='filter' and method2=='category':
