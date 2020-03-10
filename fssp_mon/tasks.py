40aa2d88e8e0d5530424d3affe98fb1abecdd4da
Aleksey Shilo <chief_exb@mail.ru>
2020-03-10 12:54:13 +0300

    Fix

--- fssp_mon/tasks.py
+++ fssp_mon/tasks.py
@@ -1,10 +1,27 @@
 # Create your tasks here
 from __future__ import absolute_import, unicode_literals
+from .models import *
+import fdb
+
 
 from celery import shared_task
 #from demoapp.models import Widget
+logging.basicConfig(format = u'%(levelname)-8s [%(asctime)s] %(message)s', level = logging.DEBUG, filename = '/home/task.log')
 
 
 @shared_task
 def add(x, y):
     return x + y
+    
+@shared_task
+def calc_view(vitrina_id,osp_id):
+     v=Vitrina.objects.filter('vitrina_id='+vitrina_id)
+     osp=Osp.objects.filter('osp_id='+osp_id)
+     sql_text = v[0].filter.sql_text
+     con = fdb.connect(host=item2['host'], database=item2['data_base'], user='SYSDBA', password=item2['password'], charset='WIN1251')
+                    
+    #calc field
+    
+    
+    
+        
