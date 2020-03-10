772e341ac670c5025330c447d3e2d767775ff5e4
Aleksey Shilo <chief_exb@mail.ru>
2020-03-11 01:50:37 +0300

    Fix

--- fssp_mon/models.py
+++ fssp_mon/models.py
@@ -83,6 +83,8 @@ class VitrinaField (models.Model):
     col18 = models.CharField(max_length=1000, null='True', blank='True', verbose_name='Col18')
     col19 = models.CharField(max_length=1000, null='True', blank='True', verbose_name='Col19')
     col20 = models.CharField(max_length=1000, null='True', blank='True', verbose_name='Col20')
+    col21 = models.DateTimeField(null='True')
+    models.CharField(max_length=1000, null='True', blank='True', verbose_name='Col21')
 
     def __str__(self):
         return 'Fields '+self.vitrina.__str__()
