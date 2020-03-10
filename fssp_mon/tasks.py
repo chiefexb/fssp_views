fbb82b0a3bdd42bf24b82f7bfc083a99d1beb0ae
Aleksey Shilo <chief_exb@mail.ru>
2020-03-10 12:55:46 +0300

    Fix

--- fssp_mon/tasks.py
+++ fssp_mon/tasks.py
@@ -2,7 +2,7 @@
 from __future__ import absolute_import, unicode_literals
 from .models import *
 import fdb
-
+import logging
 
 from celery import shared_task
 #from demoapp.models import Widget
@@ -17,6 +17,7 @@ def add(x, y):
 def calc_view(vitrina_id,osp_id):
      v=Vitrina.objects.filter('vitrina_id='+vitrina_id)
      osp=Osp.objects.filter('osp_id='+osp_id)
+     item2=osp[0]
      sql_text = v[0].filter.sql_text
      con = fdb.connect(host=item2['host'], database=item2['data_base'], user='SYSDBA', password=item2['password'], charset='WIN1251')
                     
