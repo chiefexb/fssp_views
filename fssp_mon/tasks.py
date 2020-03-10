1a8fb27b9720fbd60ed239b239d6f81a6fe0262e
Aleksey Shilo <chief_exb@mail.ru>
2020-03-10 13:13:51 +0300

    Fix

--- fssp_mon/tasks.py
+++ fssp_mon/tasks.py
@@ -19,8 +19,8 @@ def calc_view(vitrina_id,osp_id):
      osp=Osp.objects.filter(id=osp_id)
      item2=osp[0]
      sql_text = v[0].filter.sql_text
-     con = fdb.connect(host=item2['host'], database=item2['data_base'], user='SYSDBA', password=item2['password'], charset='WIN1251')
-                    
+     #con = fdb.connect(host=item2['host'], database=item2['data_base'], user='SYSDBA', password=item2['password'], charset='WIN1251')
+     return   sql_text             
     #calc field
     
     
