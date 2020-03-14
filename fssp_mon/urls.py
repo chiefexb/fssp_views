bb8f89cd0e5402a509748fa387a4a8a9da112c45
Aleksey Shilo <chief_exb@mail.ru>
2020-03-14 13:48:51 +0300

    Fix

--- fssp_mon/urls.py
+++ fssp_mon/urls.py
@@ -9,16 +9,15 @@ from fssp_mon.views import  *
 urlpatterns = [
    
     path("", views.index, name='index'),
-    path("osp", views.osp, name='osp'),
+    
     path("api/<method>", views.api, name='api'),
       path("api/<method>/<method2>", views.api2, name='api2'),
      
     path("api/", views.swagger, name='api2'),
      path("swagger.yaml", views.swagger_file, name='swagger.yaml'),
     
-    path("webhook", views.webhook, name='webhook'),
-    
-    path("osp2", views.osp_list, name='osp_list'),
+   
+   # path("osp2", views.osp_list, name='osp_list'),
     
 
    
