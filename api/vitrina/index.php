85099915ff45872a47e04d8872b33727ba6fe9ae
Aleksey Shilo <chief_exb@mail.ru>
2020-03-16 15:08:43 +0300

    Fix

--- api/vitrina/index.php
+++ api/vitrina/index.php
@@ -21,7 +21,7 @@ if(isset($_GET["spi_id"]))  {
 //$spi_id= array_search($, $array, strict_parameter)
 // r = p2.values('osp').order_by('osp').annotate(count=Count('osp'))
 if ($spi_id=='0') {
-$sql="select  (select full_name from fssp_mon_osp where id=vv.osp_id) as osp,1 as col1, count(*) as col2
+$sql="select  (select full_name from fssp_mon_osp where id=vv.osp_id) as osp,'-' as col1, count(*) as col2
          from fssp_mon_vitrinavalue vv           where vitrina_id=1 group by  osp order by osp";
 	 } else {
 $sql =  "select  (select full_name from fssp_mon_osp where id=vv.osp_id) as osp,spi as col1 , count(*) as col2         from fssp_mon_vitrinavalue vv           where vitrina_id=1 group by vv.spi, osp order by osp";
