cf4d9813a6dc451ef7b80e55cc1ba27954bd89a3
Aleksey Shilo <chief_exb@mail.ru>
2020-03-16 14:41:15 +0300

    Fix

--- api/vitrina/index.php
+++ api/vitrina/index.php
@@ -5,7 +5,7 @@
 //default-character-set = utf8
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 $mysqli = new mysqli("localhost", "fssp", "Exb021205!", "fssp");
-//A
+
 /* проверяем соединение */
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
@@ -14,16 +14,18 @@ if (mysqli_connect_errno()) {
 $spi_id='1';
 if(isset($_GET["spi_id"]))  {
     if(!empty($_GET["spi_id"])) {
-		$spi_id=$_GET["spi_id"]
+		$spi_id=$_GET["spi_id"];
 	};
 };
 
 //$spi_id= array_search($, $array, strict_parameter)
 // r = p2.values('osp').order_by('osp').annotate(count=Count('osp'))
+if ($spi_id=='0') {
 $sql="select  (select full_name from fssp_mon_osp where id=vv.osp_id) as osp,1 as col1, count(*) as col2
          from fssp_mon_vitrinavalue vv           where vitrina_id=1 group by  osp order by osp";
-//$sql =  "select  (select full_name from fssp_mon_osp where id=vv.osp_id) as osp,spi as col1 , count(*) as col2         from fssp_mon_vitrinavalue vv           where vitrina_id=1 group by vv.spi, osp order by osp";
-
+	 } else {
+$sql =  "select  (select full_name from fssp_mon_osp where id=vv.osp_id) as osp,spi as col1 , count(*) as col2         from fssp_mon_vitrinavalue vv           where vitrina_id=1 group by vv.spi, osp order by osp";
+};
 $mysqli->set_charset('utf8');
 $result = $mysqli->query($sql);
 
