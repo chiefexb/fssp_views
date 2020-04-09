739d384bf8972a1524e64648af32a7e3120d333a
Alex Pricker <alex.pricker@gmail.com>
2020-04-09 21:43:38 +0000

    Fix

--- worker/test.php
+++ worker/test.php
@@ -3,26 +3,27 @@ include 'vars.php';
     $host = '62.109.7.133:fssp';
     $username='SYSDBA';
     $password='8aJu3#7Y3j';
-    echo $vitrina3;
+    //echo $vitrina3;
     $charset='WIN1251';
     $dbh = ibase_connect($host, $username, $password, $charset);
     $stmt = $vitrina3;
     $sth = ibase_query($dbh,  $stmt);
     $count=0;
-    while ($onerow = ibasefetchrow( $sth)) {
+    while ($onerow = ibase_fetch_row( $sth)) {
         foreach($onerow as $key => $value) {
-    echo $key.": ".$value.", ";
-  }
-  echo "\n";
-}
+            $str='INSERT INTO fssp';
+            $str=$str.",".$value.", ";
+    }
+    echo $str;
+    }
     //while ($row[$count] = ibase_fetch_object($sth)) {
-      $count++;
+    //  $count++;
     //$d=count($row);
     //var_dump ($row) ;
       
     // echo $row->email, "\n";
    // }
-    echo $count;
+    //echo $count;
     ibase_free_result($sth);
     ibase_close($dbh);
 ?>
