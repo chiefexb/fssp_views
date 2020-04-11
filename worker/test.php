b60399e119afbabae6866297e795401753fafd98
Alex Pricker <alex.pricker@gmail.com>
2020-04-11 13:32:35 +0000

    Fix

--- worker/test.php
+++ worker/test.php
@@ -10,12 +10,17 @@ include 'vars.php';
     $sth = ibase_query($dbh,  $stmt);
     $count=0;
     while ($onerow = ibase_fetch_row( $sth)) {
-		 $str=vitr3;
-        foreach($onerow as $key => $value) {
-         
-            $str=$str.",".$value.", ";
-    }
-    echo $str;
+		 //$str=vitr3+"'";
+        //foreach($onerow as $key => $value) {
+           $spi=iconv('windows-1251', 'UTF-8',$onerow[0] );  
+           $col1=$onerow[1];
+           $col2=$onerow[2];
+           $col3=$onerow[3];
+  $col1=$onerow[1];
+
+ // $str=$str."' ,'".iconv('windows-1251', 'UTF-8',  $value)."', '";
+    //}
+    //echo $str;
     }
     //while ($row[$count] = ibase_fetch_object($sth)) {
     //  $count++;
