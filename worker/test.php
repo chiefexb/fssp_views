4bca77470f79c5a8cf16b786197c232f5aa13aaf
Aleksey Shilo <chief_exb@mail.ru>
2020-04-11 18:04:15 +0300

    Fix

--- worker/test.php
+++ worker/test.php
@@ -21,7 +21,7 @@ include 'vars.php';
            $col3=$onerow[3];
            $data_vozb=$onerow[4];
            $data_okon=$onerow[5];
-            $str=$vitr3."(  '".$spi."' , '". $col1."', '".$col2."', '".$col3."', '".$data_vozb."' , '".$data_okon."' )  "; 
+            $str=$vitr3."(  '".$spi."' , '". $col1."', '".$col2."', '".$col3."', '".$data_vozb."' , '".$data_okon."' );  "; 
             echo $str;
 
  // $str=$str."' ,'".iconv('windows-1251', 'UTF-8',  $value)."', '";
