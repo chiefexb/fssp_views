f532dc99ae7402b157bf0449480d5c907b039338
Aleksey Shilo <chief_exb@mail.ru>
2020-04-16 00:10:48 +0300

    Fix

--- api/vitrina/index.php
+++ api/vitrina/index.php
@@ -43,9 +43,10 @@ if(isset($_GET["date2"]))  {
 //and data_vozb>='" . date1 ."' and  data_vozb<='" . date2 ."'
 if ($vitrina_id=='1') {
 $per="data_okon>='" . $date1 ."' and  data_okon<='" . $date2 ."' and";
+$per2="data_okon>='" . $date1 ."' and  data_okon<='" . $date2 "' ";
 $sql_count="
 SUM(CASE WHEN (data_vozb>='" . $date1 ."' and  data_vozb<='" . $date2 ."' ) THEN 1 ELSE 0 END ) as col3,
-
+SUM(CASE WHEN (".$per2. "  ) THEN 1 ELSE 0 END ) as col4,
 SUM(CASE WHEN (".$per. " col2='47' and col3='1' and col4='1' ) THEN 1 ELSE 0 END ) as col5,
 SUM(CASE WHEN (".$per. " col2='47' and col3='1' and col4='2') THEN 1 ELSE 0 END  ) as col6,
 SUM(CASE WHEN (".$per. " col2='47' and col3='1' and col4='8') THEN 1 ELSE 0 END  ) as col7,
@@ -68,7 +69,7 @@ $sql="select  (select full_name from fssp_mon_osp where id=vv.osp_id) as osp,'-'
 
          from fssp_mon_vitrinavalue vv           where vitrina_id=1 group by  osp order by osp";
 	 } else {
-$sql =  "select  (select full_name from fssp_mon_osp where id=vv.osp_id) as osp,spi as col1 , count(spi) as col4 , 
+$sql =  "select  (select full_name from fssp_mon_osp where id=vv.osp_id) as osp,spi as col1 ,  
   " .$sql_count ."     from fssp_mon_vitrinavalue vv           where vitrina_id=1 and data_okon>='" . $date1 ."' and  data_okon<='" . $date2 ."'group by vv.spi, osp order by osp";
 }
 
