29a486261c70c039f1b72e5793cf288663d2989d
Aleksey Shilo <chief_exb@mail.ru>
2020-04-17 15:16:58 +0300

    Fix

--- worker/setup_osp.php
+++ worker/setup_osp.php
@@ -8,18 +8,25 @@ var_dump( $mysql_db[0]["host"]);
  $mysqli->set_charset('utf8');
  
  
-$result = $mysqli->query("select * from fssp_mon_osp where osp_id=1");
 
 
-if (!$result) {
-    echo "Could not successfully run query ($sql) from DB: "; // . mysql_error();
-    exit;
-}
 
-$row=$result -> fetch_all(MYSQLI_ASSOC);
+
+
+
 
 for($i = 0; $i < count($osp); ++$i) {
-   var_dump ($osp[$i]['osp_id'] );
+   $result = $mysqli->query("select * from fssp_mon_osp where osp_id=". $osp[$i]['osp_id']  );
+   if (!$result) {
+    echo "Could not successfully run query ($sql) from DB: "; // . mysql_error();
+    exit;
+     }
+    $row=$result -> fetch_all(MYSQLI_ASSOC);
+    if (count($row)=0 ) {
+		echo "osp not found";
+	} 
+    
+   
 };
 
 ?>
