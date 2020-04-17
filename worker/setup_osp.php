26328a41a1500de94923d52a0eb791728db3cab3
Aleksey Shilo <chief_exb@mail.ru>
2020-04-17 15:06:38 +0300

    Fix

--- worker/setup_osp.php
+++ worker/setup_osp.php
@@ -6,11 +6,20 @@ var_dump( $mysql_db[0]["host"]);
 
  $mysqli = new mysqli($mysql_db[0]["host"], $mysql_db[0]["username"], $mysql_db[0]["password"], $mysql_db[0]["db"]) ;
  $mysqli->set_charset('utf8');
+ 
+ 
+$result = $mysqli->query("select * from fssp_mon_osp where osp_id=1");
 
- if ($mysqli->query("select * from fssp_mon_osp")) {
-     echo "New record delete successfully";
-     } else {
-     echo "Error: " . $mysqli->error;
-     }
+
+if (!$result) {
+    echo "Could not successfully run query ($sql) from DB: "; // . mysql_error();
+    exit;
+}
+
+$row=$result -> fetch_all(MYSQLI_ASSOC);
+
+for($i = 0; $i < count($osp); ++$i) {
+   echo $osp[$i]['osp_id'] 
+};
 
 ?>
