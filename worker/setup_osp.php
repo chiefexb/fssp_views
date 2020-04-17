ab4a83e4a9a6a049348f82d30676e7f6749004af
Alex Pricker <alex.pricker@gmail.com>
2020-04-17 11:52:32 +0000

    Fix

--- worker/setup_osp.php
+++ worker/setup_osp.php
@@ -1,13 +1,16 @@
 <?php
 include 'config.php';
 //Проверяем есть ли район в таблице
-var_dump( mysql_db);
- //$mysqli = new mysqli(mysql_db[0]=>host, mysql_db[0]=>user, mysql_db[0]=>password, mysql_db[0]=>db);
- // $mysqli->set_charset('utf8');
- // if ($mysqli->query("select * from fssp_mon_osp") {
- //    echo "New record delete successfully";
- //    } else {
- //    echo "Error: " . $mysqli->error;
- //    }
+//var_dump( $osp);
+var_dump( $mysql_db[0]["host"]);
+
+ $mysqli = new mysqli($mysql_db[0]["host"], $mysql_db[0]["username"], $mysql_db[0]["password"], $mysql_db[0]["db"]) ;
+ $mysqli->set_charset('utf8');
+
+ if ($mysqli->query("select * from fssp_mon_osp")) {
+     echo "New record delete successfully";
+     } else {
+     echo "Error: " . $mysqli->error;
+     }
 
 ?>
