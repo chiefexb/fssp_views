a1442431c6e2a00f19ce3fbad4b6da0ca255d2f3
Aleksey Shilo <chief_exb@mail.ru>
2020-04-17 14:54:28 +0300

    Fix

--- worker/config_example.php
+++ worker/config_example.php
@@ -1,21 +1,21 @@
+<?php
 $osp = [[
+    "name"=> "Урупский РО",
+    "short_name" => "Урупский РО",
     "osp_id" => 1,
-    "host"=> "62.109.adress.ip:fssp"
+    "host" => "62.109.7.133:fssp",
+    "username" => "SYSDBA",
+    "password" => ""
+   ], 
+[ "osp_id" => 1,
+    "host"=> "62.109.7.133:fssp",
     "username"=>"SYSDBA",
-    "password"
-   ],
-[
-    "osp_id" => 1,
-    "host"=> "62.abc.y.xxx:fssp"
-    "username"=>"SYSDBA",
-     "password"
+    "password"=>"SYSDBA"]];
 
-]];
-
-mysql_db=[[ 
+$mysql_db = [[ 
     "host" => "localhost",
     "db"=>"fssp", 
     "username"=> "fssp",
     "password"=> ""
-]]
-host, $username, $password, $charset
+]];
+?>
