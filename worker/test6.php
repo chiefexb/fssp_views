2d639b7e8bf76cb92ce8831d396d26ea8eaf89ad
Aleksey Shilo <chief_exb@mail.ru>
2020-04-21 14:34:14 +0300

    Fix

--- worker/test6.php
+++ worker/test6.php
@@ -29,8 +29,8 @@ $rdb_size_procent =$json_a["rdb_size_available"]/$json_a["rdb_size_total"];
 
 
   
-  $sql2="select id from fssp_mon_osp where osp_id=.$osp_id;
-  $sql3="select id from fssp_mon_vitrina where vitrina_id=5;
+  $sql2="select id from fssp_mon_osp where osp_id=".$osp_id;
+  $sql3="select id from fssp_mon_vitrina where vitrina_id=5";
   
     
     if ($mysqli->query("DELETE FROM fssp_mon_vitrinavalue where vitrina_id=(". $sql3. ") and osp_id=(" . $sql2.")" ) === TRUE) {
