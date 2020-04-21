130636802815566b229e0e2665b795dc6461ce98
Aleksey Shilo <chief_exb@mail.ru>
2020-04-21 18:21:50 +0300

    Fix

--- api/vitrina/index.php
+++ api/vitrina/index.php
@@ -138,7 +138,7 @@ $sql =  "select  (select full_name from fssp_mon_osp where id=vv.osp_id) as osp,
   " .$sql_count ."     from fssp_mon_vitrinavalue vv           where vitrina_id=(select id from fssp_mon_vitrina where vitrina_id=1) group by vv.spi, osp order by osp";
 }
 	
- }  else if ($vitrina_id=='4') {
+ }  else if ($vitrina_id=='5') {
 	 $sql_count  ="col23 as col3,  
 	               col28 as col4 ,
 	               col29 as col5,
