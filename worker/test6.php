47133a1386331289111f1ea34c3d2070685b13d2
Aleksey Shilo <chief_exb@mail.ru>
2020-04-21 14:27:51 +0300

    Fix

--- worker/test6.php
+++ worker/test6.php
@@ -1,19 +1,20 @@
 <?php
 include 'vars.php';
 include 'config.php';
+$osp_id=1; 
+
   $jj = file_get_contents("/var/www/portal/osp_1.json"); 
   $json_a = json_decode($jj, true);
   var_dump($json_a)  ;
 // mon_file_name 
-//col3 cpu_info
-//cpu_core col27
- // "mem_total": col28
-//  "root_size_available": col 25
-//  "root_size_available": "126286888960",
-//  "rdb_size_available": "27380080640", 26
- // "rdb_size_total": "82957524992"
+
+$cpu_info=$json_a[$json_a["cpu_info"]; 
+$cpu_core=$json_a[$json_a["cpu_core"]; 
+
+$mem_total=$json_a["$json_a["rdb_size_available"];
+
 $root_size_procent =$json_a["rdb_size_available"]/$json_a["rdb_size_total"];
-$root_size_procent =$json_a["rdb_size_available"]/$json_a["rdb_size_total"];
+$rdb_size_procent =$json_a["rdb_size_available"]/$json_a["rdb_size_total"];
 
 
 //var_dump( $mysql_db[0]["host"]);
@@ -22,24 +23,38 @@ $root_size_procent =$json_a["rdb_size_available"]/$json_a["rdb_size_total"];
  $mysqli->set_charset('utf8');
  
 
-$vitr6=" INSERT INTO fssp_mon_vitrinavalue (vitrina_id, osp_id, col23, data_vozb) VALUES ";
 
 
-
-
-
-
-$sql="INSERT INTO fssp_mon_vitrina_value (filter_id,vitrina_id,name,date_actual,calc_field_name,custom_fields) VALUE  (";
 
 
 
   
-		$sql2= $sql. "1, ". $vitrina[$i]['vitrina_id']. ", '".  $vitrina[$i]['name'] ."','2020-01-01',1,1 );"  ;
-	//	$rez=$mysqli->query($sql2) ; 
+  $sql2="select id from fssp_mon_osp where osp_id=".$osp_id;
+  $sql3="select id from fssp_mon_vitrina where vitrina_id=5;
+  
+    
+    if ($mysqli->query("DELETE FROM fssp_mon_vitrinavalue where vitrina_id=(".sql3.") and osp_id=(".$sql2.")" ) === TRUE) {
+     echo "New record delete successfully\n";
+    } else {
+    echo "Error: " . $mysqli->error;
+     }
+ 
+
+     $vitr6=" INSERT INTO fssp_mon_vitrinavalue (vitrina_id, osp_id, col23,col28,col29,col25,col26 data_vozb) VALUES ";
+      $col23= $cpu_core;
+      $col28 =$cpu_core;
+      $col29=$mem_total;
+      $col25=$root_size_procent;
+      $col26=$rdb_size_procent
+     
+     $str=$vitr6 . "((".$sql3."),(".$sql2."),  ". $cpu_info.", " . $data_vozb. " );  "; 
+	       
+		
+		$rez=$mysqli->query( $str) ; 
 		
 	if (!$rez) {
-	  echo  $sql2 ."\n";
-       echo "Could not successfully run query ". $sql2. "-"  . mysql_error()  ;
+	  echo  $str ."\n";
+       echo "Could not successfully run query ". $str. "-"  . mysql_error()  ;
     exit ;  } 	 else {
 		var_dump ($row);
 		}
