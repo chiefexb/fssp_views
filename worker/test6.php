ff5ece8f3190975eb909bcc69eb8635d2b7739f9
Aleksey Shilo <chief_exb@mail.ru>
2020-04-21 19:46:02 +0300

    Fix

--- worker/test6.php
+++ worker/test6.php
@@ -11,9 +11,9 @@ $osp_id=1;
 $cpu_info=$json_a["cpu"]; 
 $cpu_core=$json_a["cpu_core"]; 
 
-$mem_total=$json_a["rdb_size_available"];
+$mem_total=$json_a["memtotal"];
 
-$root_size_procent =$json_a["rdb_size_available"]/ $json_a["rdb_size_total"];
+$root_size_procent =$json_a["root_size_available"]/ $json_a["root_size_total"];
 $rdb_size_procent =$json_a["rdb_size_available"]/ $json_a["rdb_size_total"];
   var_dump($root_size_procent)  ;
   var_dump($rdb_size_procent )  ;
