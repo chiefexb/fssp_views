620f54de5263c7e928f3d0323fcce5abc17b8571
Aleksey Shilo <chief_exb@mail.ru>
2020-04-21 13:37:23 +0300

    Fix

--- worker/test6.php
+++ worker/test6.php
@@ -3,12 +3,11 @@ include 'vars.php';
 include 'config.php';
   $jj = file_get_contents("/var/www/portal/osp_1.json"); 
   $json_a = json_decode($jj, true);
-  var_dump($json_a)  
+  var_dump($json_a)  ;
 // mon_file_name 
 
 
-//Проверяем есть ли район в таблице
-var_dump( $json_a);
+
 //var_dump( $mysql_db[0]["host"]);
 
  $mysqli = new mysqli($mysql_db[0]["host"], $mysql_db[0]["username"], $mysql_db[0]["password"], $mysql_db[0]["db"]) ;
