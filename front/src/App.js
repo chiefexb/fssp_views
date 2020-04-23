2df381ffaa8b7388a9d151f26aaae7e181da634d
Aleksey Shilo <chief_exb@mail.ru>
2020-04-23 15:06:18 +0300

    Fix

--- front/src/App.js
+++ front/src/App.js
@@ -609,10 +609,10 @@ if (!this.state.loaded7) {
 
  componentDidMount() {
 	 	
-	//this.timerID = setInterval(
-   //   () => this.renderTable(),
-   //  1000
-   //  );
+	this.timerID = setInterval(
+      () => this.renderTable(),
+     1000
+     );
     //?vitrina_id=1&counter_id=1&spi_id=0  
     fetch(`api/vitrina?vitrina_id=${this.state.vitrina_id}&counter_id=${this.state.counter_id}&spi_id=${this.state.spi_id}&date1=${this.state.selectedDate1}&date2=${this.state.selectedDate2}`)  
     //fetch(`api/vitrina?vitrina_id=${this.state.vitrina_id}&counter_id=${this.state.counter_id}&spi_id=${this.state.spi_id}`)
